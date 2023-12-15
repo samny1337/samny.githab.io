413bfa692dd80583fb3f018f93d89ec7ef9886ce
Nikita Kutepov <camelcloud7@gmail.com>
2023-12-15 17:26:40 +0200

    домашка по typescript v2

--- src/homeworks/ts1/1_base.ts
+++ src/homeworks/ts1/1_base.ts
@@ -1,4 +1,4 @@
-/**
+/** 
  * Нужно превратить файл в ts и указать типы аргументов и типы возвращаемого значения
  * */
 export const removePlus = (string: string) : string => string.replace(/^\+/, '');
